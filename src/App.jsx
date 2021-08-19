6656445c752d20d76172c2211a61969ad4562725
aritonkaramani <ariton.karamani@gmail.com>
2021-08-20 01:20:33 +0200

    Added icons to all weathers (some duplicates) -> TODO: Add icons based on time at the clocation, fix forecast(async/await issues atm)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Clouds from '../src/Assets/PNG/512/cloudy.png';
 import Rain from '../src/Assets/PNG/512/rain.png';
+import Sun from '../src/Assets/PNG/512/day_clear.png';
+import Thunder from '../src/Assets/PNG/512/thunder.png';
+import Snow from '../src/Assets/PNG/512/snow.png';
+import Fog from '../src/Assets/PNG/512/fog.png';
 
 function App() {
 
@@ -12,6 +16,9 @@ function App() {
     baseNoPageBreak: "api.openweathermap.org/data/2.5/weather?q=Malmo&units=metric&appid=29b3245b274832af2341a683f1348817"
   }
   const [query,setQuery] = useState('');
+  const [forQuery,setForQuery] = useState('');
+  const [cords,setCords] = useState({});
+  const [forecast,setForecast] = useState({});
   const [weather,setWeather] = useState({});
   
 
@@ -26,12 +33,37 @@ function App() {
 
     let icon = null; 
     switch (key) {
+      case "Drizzle":
       case "Rain":
       icon = Rain;
       break;
       case "Clouds":
       icon = Clouds;
-    
+      break;
+      case "Sunny":
+      icon = Sun;
+      break;
+      case "Clear":
+      icon = Sun;
+      break;
+      case "Thunderstorm":
+      icon = Thunder;
+      break;
+      case "Snow":
+      icon = Snow;
+      break;
+      case "Mist":
+      case "Smoke":
+      case "Haze":
+      case "Dust":
+      case "Fog":
+      case "Sand":
+      case "Dust":
+      case "Ash":
+      case "Squall":
+      case "Tornado":
+      icon = Fog;
+      break;
       default:
         break;
     }
@@ -40,7 +72,7 @@ function App() {
   }
 
 
-  const onEnterPress = evt => {
+  const onEnterPress = async evt => {
     if(evt.key === "Enter"){
       axios.get(`https://${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
       .then(res => {
@@ -48,8 +80,22 @@ function App() {
         setQuery('')
 
       })
-
+      .then(
+        axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${api.key}`)
+        .then(result => {
+          setCords(result.data) // cords
+          if(cords.length != null) {
+          axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${cords[0].lat}&lon=${cords[0].lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${api.key}`)
+          .then(result => {
+            setForecast(result.data)
+            console.log(result.data)
+          })
+        }
+          
+        })
+        )
   }
+   
 }
 
   let date = String(new window.Date());
